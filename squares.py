3d85f9a66f954e32be72089408fd60e21289a668
Godfred Adjei-Darko <gadjeid@siue.edu>
2024-01-26 12:19:42 -0600

    changing wording on line 13

--- squares.py
+++ squares.py
@@ -10,4 +10,4 @@ while not valid_input:
         print(f"{result} is the exact result of {user_input} squared!")
         valid_input = True
     else:
-        print("Invalid input. Please enter a numerical value.")
+        print("Input is not a number. Please enter a number.")
